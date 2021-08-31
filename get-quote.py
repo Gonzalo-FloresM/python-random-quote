97fb5941f1a402330a142afc45da61fc98f12efa
Gonzalo-FloresM <floresgo42@gmail.com>
2021-08-31 20:48:13 -0300

    Tutorial Completion

--- get-quote.py
+++ get-quote.py
@@ -8,8 +8,16 @@ def primary():
   f.close()
   last = len(quotes) - 1
   rnd = random.randint(0, last)
+  rnd2 = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd] ,end = '')
+  print(quotes[rnd2])
+
+  print("Hello there!", end = '')
+  print("It is a great day.")
+
+  quotes.append("Explore Python on your own")
+  print(quotes[len(quotes)-1])
 
 if __name__== "__main__":
   primary()
